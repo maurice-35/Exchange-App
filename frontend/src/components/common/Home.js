e8f867bccc61ab7dedaaa29977647b4e6ea4d24d
mauricekollewe <kollewem.35@gmail.com>
2021-10-25 23:17:36 +0100

    update home

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,23 +1,63 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
 const Home = () => {
 
+	const [USD, setUSD] = useState('')
+	const [currency, setCurrency] = useState('')
+
+
 	useEffect(() => {
-		
+
 		const getData = async () => {
 			const { data } = await axios.get('https://v6.exchangerate-api.com/v6/df125311940707489475a75a/latest/USD')
 			console.log('DATA', data)
 		}
 		getData()
-	}, )
-	
+	})
 
+  const handleChange = (event) => {
+		setUSD (event.target.value)
+		setCurrency (event.target.value)
+	}
 
 	return (
 		<>
-		<h1>This is Home</h1>
+			<div className="container">
+				<div className="field" id="first">
+					<form className="column is-half is-offset-half box">
+						<select value={USD} onChange={handleChange}>
+							<option value="USD">USD</option>
+							<option value="GBP">GBP</option>
+							<option value="EUR">EUR</option>
+						</select>
+					</form>
+					<form>
+						<select value={currency} onChange={handleChange}>
+							<option value="USD">USD</option>
+							<option value="GBP">GBP</option>
+							<option value="EUR">EUR</option>
+						</select>
+					</form>
+				</div>
+			</div>
+			<div className="container">
+				<div className="field">
+					<input
+						type="text"
+						placeholder="Exchange rate"
+						name="currency"
+					/>
+				</div>
+				<div className="field">
+					<input
+						type="number"
+						placeholder="Amount"
+						name="currency"
+					/>
+				</div>
+			</div>
 		</>
 	)
 }
