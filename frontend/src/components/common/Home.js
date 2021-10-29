a5ec3a66b3334812bc525d29066b2b5e5f984f51
Maurice Kollewe T <84001897+maurice-35@users.noreply.github.com>
2021-10-29 18:14:08 +0100

    Update Home.js

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,38 +1,14 @@
 import React from 'react'
-// import axios from 'axios'
-// import CurrencyCard from './currencies/CurrencyCard'
+
 
 
 const Home = () => {
 
-	// const [currencies, setCurrencies] = useState([])
-	// const [hasError, setHasError] = useState()
-
-
-	// useEffect(() => {
-
-	// 	const getData = async () => {
-	// 		try {
-	// 		const { data } = await axios.get('https://api.coinbase.com/v2/currencies')
-	// 		// console.log('DATA', data)
-	// 		setCurrencies(data)
-	// 		} catch {
-
-	// 		setHasError(true)
-	// 		console.log('Error', hasError)	
-	// 	}
-	// }
-	// 	getData()
-	// }, )
-
-  // const handleChange = (event) => {
-	// 	setCurrencies (event.target.value)
-	// }
-
+	
 	
 
-	return (
-<section className="hero is-fullheight-with-navbar is-success">
+return (
+  <section className="hero is-fullheight-with-navbar is-success">
       <div className="hero-body">
 			<div class="ball">
         <div className="container">
